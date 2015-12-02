8246e3ce24341179e3e1ae67d6fbf44b60d4eb03
RickYeh <RickYeh@users.noreply.github.com>
2015-12-02 04:37:54 -0800

    Added variable for stat selection

--- kobe/kobe.js
+++ kobe/kobe.js
@@ -1,80 +1,87 @@
-d3.csv('kobeBryant.csv', type, function(error, data) {
+ function drawChart(queryStat) {
 
-    var width = 1000;
-    var barHeight = 30;
-    var height = (barHeight + 5) * data.length;
-    var padding = 50;
+    d3.csv('kobeBryant.csv', type, function(error, data) {
 
-    var barChart = d3.select('#container')
-        .append('svg:svg')
-        .attr('width', width)
-        .attr('height', height)
-        .attr('style', 'background-color: #EEE');
+        var width = 1000;
+        var barHeight = 30;
+        var height = (barHeight + 5) * data.length;
+        var padding = 50;
 
-    // X Scale
-    var x = d3.scale.linear()
-        .domain([0, d3.max(data, function(d) {
-            return +d.PTS + 1;
-        })])
-        .rangeRound([0, width]);
+        var barChart = d3.select('#container')
+            .append('svg:svg')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('style', 'background-color: #EEE');
 
-    // Y Scale
-    var y = d3.scale.linear()
-        .domain([0, data.length])
-        .range([0, height]);
+        // X Scale
+        var x = d3.scale.linear()
+            .domain([0, d3.max(data, function(d) {
+                return +d[queryStat];
+            })])
+            .rangeRound([0, width]);
 
-    barChart.selectAll('rect')
-        .data(data)
-        .enter()
-        .append('svg:rect')
-        .attr('x', 0)
-        .attr('y', function(d, i) {
-            return y(i);
-        })
-        .attr('height', barHeight)
-        .attr('width', function(d) {
-            return x(d.PTS);
-        })
-        .attr('fill', '#2d578b');
+        // Y Scale
+        var y = d3.scale.linear()
+            .domain([0, data.length])
+            .range([0, height]);
 
-    barChart.selectAll('textPlayer')
-        .data(data)
-        .enter()
-        .append('svg:text')
-        .attr('x', 0)
-        .attr('y', function(d, i) {
-            return y(i);
-        })
-        .attr('dx', '1em')
-        .attr('dy', '1.25em')
-        .attr('text-anchor', 'left')
-        .text(function(d) {
-            return d.Season;
-        })
-        .attr('fill', 'white')
-        .attr('style', 'font-size: 16; font-family: Helvetica, sans-serif');
+        barChart.selectAll('rect')
+            .data(data)
+            .enter()
+            .append('svg:rect')
+            .attr('x', 0)
+            .attr('y', function(d, i) {
+                return y(i);
+            })
+            .attr('height', barHeight)
+            .attr('width', function(d) {
+                return x(d[queryStat]);
+            })
+            .attr('fill', '#2d578b');
 
-    barChart.selectAll('textPts')
-        .data(data)
-        .enter()
-        .append('svg:text')
-        .attr('x', function(d) { return x(d.PTS); }) 
-        .attr('y', function(d, i) {
-            return y(i);
-        })
-        .attr('dx', '-6em')
-        .attr('dy', '1.25em')
-        .attr('text-anchor', 'right')
-        .text(function(d) {
-            return d.PTS + ' PPG';
-        })
-        .attr('fill', 'white')
-        .attr('style', 'font-size: 16; font-family: Helvetica, sans-serif');
-});
+        barChart.selectAll('textSeason')
+            .data(data)
+            .enter()
+            .append('svg:text')
+            .attr('x', 0)
+            .attr('y', function(d, i) {
+                return y(i);
+            })
+            .attr('dx', '1em')
+            .attr('dy', '1.25em')
+            .attr('text-anchor', 'left')
+            .text(function(d) {
+                return d.Season;
+            })
+            .attr('fill', 'white')
+            .attr('style', 'font-size: 16; font-family: Helvetica, sans-serif');
 
+        barChart.selectAll('textStats')
+            .data(data)
+            .enter()
+            .append('svg:text')
+            .attr('x', function(d) { return x(d[queryStat]); }) 
+            .attr('y', function(d, i) {
+                return y(i);
+            })
+            .attr('dx', '-4em')
+            .attr('dy', '1.25em')
+            .attr('text-anchor', 'right')
+            .text(function(d) {
+                return d[queryStat] + ' ' + queryStat;
+            })
+            .attr('fill', 'white')
+            .attr('style', 'font-size: 16; font-family: Helvetica, sans-serif');
+    });
+}
 $(document).ready(function() {
     $('#ButtonPTS').click(function() {
+        drawChart('PTS');
         console.log('PTS');
+    });
+    $('#Button3PM').click(function() {
+        drawChart('3PM');
+        console.log('3PM');
     });
 });
 
